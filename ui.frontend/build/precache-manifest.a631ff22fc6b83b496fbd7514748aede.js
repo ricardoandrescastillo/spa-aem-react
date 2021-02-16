a12dce382b90bb009afaf490222280b5c710bfd5
Ricardo Castillo <rcastillo@H11429.ad.hugeinc.com>
2021-02-16 18:32:13 -0500

    first archetype 25

---
+++ ui.frontend/build/precache-manifest.a631ff22fc6b83b496fbd7514748aede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4f2b02dfe842cc28c2edc7aebeac00",
+    "url": "/etc.clientlibs/spa-react-aem/clientlibs/clientlib-react/resources/index.html"
+  },
+  {
+    "revision": "49e6c1c65d016b0b3161",
+    "url": "/etc.clientlibs/spa-react-aem/clientlibs/clientlib-react/resources/static/css/main.e57bbe8a.chunk.css"
+  },
+  {
+    "revision": "44566443a08be3cb1c42",
+    "url": "/etc.clientlibs/spa-react-aem/clientlibs/clientlib-react/resources/static/js/2.6e9827ed.chunk.js"
+  },
+  {
+    "revision": "a997066f5a8da487bf2934a8b8fd42fb",
+    "url": "/etc.clientlibs/spa-react-aem/clientlibs/clientlib-react/resources/static/js/2.6e9827ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e6c1c65d016b0b3161",
+    "url": "/etc.clientlibs/spa-react-aem/clientlibs/clientlib-react/resources/static/js/main.3833befc.chunk.js"
+  },
+  {
+    "revision": "86c74ce4b2915d0d7b7b",
+    "url": "/etc.clientlibs/spa-react-aem/clientlibs/clientlib-react/resources/static/js/runtime-main.988b8cca.js"
+  }
+]);
